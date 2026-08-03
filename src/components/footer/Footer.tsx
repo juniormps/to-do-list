3d48742fde9c8e29033203c2717d1948bfd1b038
Marcio Junior <mps.junior@hotmail.com>
2026-08-03 18:22:40 -0300

    refactor(layout): remove divs wrapper desnecessárias do Header e Footer
    - elimina <div> externa do componente Header, retornando <header> diretamente
    - elimina <div> externa do componente Footer, retornando <footer> diretamente
    - melhora a semântica HTML e reduz a profundidade da árvore DOM

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,13 +2,11 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
     return (
-        <div>
-            <footer className={styles.footer}>
-                <p>
-                    <span>Desenvolvido por Marcio Junior</span> @2026
-                </p>
-            </footer>
-        </div>
+        <footer className={styles.footer}>
+            <p>
+                <span>Desenvolvido por Marcio Junior</span> @2026
+            </p>
+        </footer>
     );
 };
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,9 @@ import styles from "./Header.module.css";
 
 const Header = () => {
     return (
-        <div>
-            <header className={styles.header}>
-                <h1>to-do list</h1>
-            </header>
-        </div>
+        <header className={styles.header}>
+            <h1>to-do list</h1>
+        </header>
     );
 };
 
